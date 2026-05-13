317f873e44b3f3e260038296232b1ae0e9fde081
Dev1994eke <Devime7289128912@outlook.com>
2026-05-13 08:20:31 +0200

    Update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -1,3 +1,13 @@
 local songs = {
   ["Rush E"] = 'loadstring(game:HttpsGet("https://raw.githubusercontent/Dev1994eke/PianoPlayer/refs/heads/main/musics/RushE.lua')
 }
+-- Instances
+ screengui = Instance.new("ScreenGui")
+frame = Instance.new("Frame")
+frameload = Instance.new("Frame")
+frameloadtext = Instance.new("TextLabel")
+frameloadbar = Instande.new("Frame")
+closebtn = Instance.new("TextButton")
+frameMusic = Instance.new("ScrollingFrame")
+frameMusicTemp = Instance.new("Frame")
+MusicText = Instance.new("TextLabel",frameMusicTemp)
