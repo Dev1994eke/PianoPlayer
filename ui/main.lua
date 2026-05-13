293d4085d61e7bb79f52a6f14957760d9b97ce50
Dev1994eke <Devime7289128912@outlook.com>
2026-05-13 08:28:42 +0200

    Update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -11,3 +11,13 @@ closebtn = Instance.new("TextButton")
 frameMusic = Instance.new("ScrollingFrame")
 frameMusicTemp = Instance.new("Frame")
 MusicText = Instance.new("TextLabel",frameMusicTemp)
+MusicPlayBtn = Instance.new("TextButton",frameMusicTemp)
+uiCorner = Instance.new("UICorner",frame)
+
+
+-- Propieties
+screengui.Name = "MAK930"
+screengui.Parent = game.Players.LocalPlayer.PlayerGUI
+
+frame.Name = "MainUI"
+frame.Parent = screengui
